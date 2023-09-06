78535a6a333ad2f7dced12d9b326f0844fad3cf0
ShingoKumada <119488635+shingo-kumada@users.noreply.github.com>
2023-09-07 08:22:04 +0900

    Update linux_build.sh

--- packages/shell/bin/linux_build/linux_build.sh
+++ packages/shell/bin/linux_build/linux_build.sh
@@ -40,4 +40,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 ### change login shell to zsh ###
 /home/linuxbrew/.linuxbrew/bin/zsh | sudo tee -a /etc/shells
-sudo chsh -s "$(/home/linuxbrew/.linuxbrew/bin/zsh)" "${USER}"
+sudo chsh -s "/home/linuxbrew/.linuxbrew/bin/zsh" "${USER}"
