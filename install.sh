0e7d32d9ec482683581fd153593b808099d1503f
Shingo Kumada <119488635+shingo-kumada@users.noreply.github.com>
2023-12-22 18:58:09 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ if [ ! -f ~/.antigen/antigen.zsh ]; then
 fi
 
 ### install vim-plug ###
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ### change login shell to zsh ###
 sudo chsh -s "/usr/bin/zsh" "${USER}"
